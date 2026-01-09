72c5971cc9d8bd1456379ac65d611be30f2ecd08
Izaias Neto <izaiasmneto@gmail.com>
2026-01-09 09:33:47 -0300

    feat: add new AuthController with login and validation

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -5,5 +5,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
-    Route::apiResource('tickets', TicketController::class);
+    Route::middleware('auth:sanctum')->apiResource('tickets', TicketController::class);
 });
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
