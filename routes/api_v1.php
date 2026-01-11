af7fee874e73a74cf21fe036de16184508317112
Izaias Neto <izaiasmneto@gmail.com>
2026-01-11 11:55:39 -0300

    feat: add ticketResource and UserResource

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -1,11 +1,13 @@
 <?php
 
 use App\Http\Controllers\Api\V1\TicketController;
+use App\Http\Controllers\Api\V1\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('v1')->group(function () {
     Route::middleware('auth:sanctum')->apiResource('tickets', TicketController::class);
+    Route::middleware('auth:sanctum')->apiResource('users', UserController::class);
 });
 
 Route::get('/user', function (Request $request) {
