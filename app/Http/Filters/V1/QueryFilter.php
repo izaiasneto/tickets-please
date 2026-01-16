c8b6b23fdba58459653da627795310e3cd49df5d
Izaias Neto <izaiasmneto@gmail.com>
2026-01-16 20:58:32 -0300

    feat: add sorting data to ticket and user filters

--- app/Http/Filters/V1/QueryFilter.php
+++ app/Http/Filters/V1/QueryFilter.php
@@ -8,20 +8,10 @@ use Illuminate\Http\Request;
 abstract class QueryFilter
 {
     protected $builder;
+    protected $sortable = [];
 
     public function __construct(protected Request $request)
     {}
-
-    public function filter($arr)
-    {
-        foreach ($arr as $key => $value) {
-            if (method_exists($this, $key)) {
-                $this->$key($value);
-            }
-        } 
-
-        return  $this->builder;
-    }
 
     public function apply(Builder $builder): Builder
     {
@@ -34,5 +24,42 @@ abstract class QueryFilter
         }
 
         return $builder;
+    }
+
+    protected function filter($arr)
+    {
+        foreach ($arr as $key => $value) {
+            if (method_exists($this, $key)) {
+                $this->$key($value);
+            }
+        } 
+
+        return  $this->builder;
+    }
+
+    public function sort($value) 
+    {
+        $sortAttributes = explode(',', $value);
+       
+        foreach ($sortAttributes as $sortAttribute) {
+            $direction = 'asc';
+            
+            if(strpos($sortAttribute, '-') === 0) {
+                $direction = 'desc';
+                $sortAttribute = substr($sortAttribute, 1);
+            }
+
+            if (!in_array($sortAttribute, $this->sortable) && !array_key_exists($sortAttribute, $this->sortable)) {
+                continue;
+            }
+
+            $columnName = $this->sortable[$sortAttribute] ?? null;
+
+            if ($columnName === null) {
+                $columnName = $sortAttribute;
+            }
+
+            return $this->builder->orderBy($columnName, $direction);
+        }
     }
 }
