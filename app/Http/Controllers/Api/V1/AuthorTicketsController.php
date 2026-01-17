42ba1926de1b24c90d3e94f35405c66d6b789cef
Izaias Neto <izaiasmneto@gmail.com>
2026-01-17 19:33:10 -0300

    feat: create resources

--- app/Http/Controllers/Api/V1/AuthorTicketsController.php
+++ app/Http/Controllers/Api/V1/AuthorTicketsController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Filters\V1\TicketFilter;
+use App\Http\Requests\Api\V1\StoreTicketRequest;
 use App\Http\Resources\V1\TicketResource;
 use App\Models\Ticket;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthorTicketsController extends Controller
@@ -17,5 +19,20 @@ class AuthorTicketsController extends Controller
                 ->filter($filters)
                 ->paginate()
         );
+    }
+
+     /**
+     * Store a newly created resource in storage.
+     */
+    public function store(User $author, StoreTicketRequest $request)
+    {
+        $model = [
+            'title' => $request->input('data.attributes.title'),
+            'description' => $request->input('data.attributes.description'),
+            'status' => $request->input('data.attributes.status'),
+            'user_id' => $author->id
+        ];
+
+        return new TicketResource(Ticket::create($model));
     }
 }
